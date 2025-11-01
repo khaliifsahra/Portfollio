368af695183b994bc75f91532b1ff65c3b97a389
Zahra Khaliif <youremail@example.com>
2025-11-01 09:43:57 -0700

    last

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+
 import "./About.css";
 
-import profile_image2 from "..//../assets/Imgs/profile_image2.svg";
+import profile_image2 from "../../assets/Imgs/profile_image2.svg";
 import pp from "../../assets/Imgs/pp.png";
 const About = () => {
   return (
